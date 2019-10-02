de7e04e1bd9b1b7ca45882b9d20ea873420d9bd0
Scott Brimhall <scott@brimhallmail.com>
2019-10-02 01:44:54 -0600

    Add verbose output

--- tasks/generate_types.rb
+++ tasks/generate_types.rb
@@ -10,18 +10,27 @@ def generate_types(environment)
 
   stdout, stderr, status = Open3.capture3(*cmd) # rubocop:disable Lint/UselessAssignment
   raise Puppet::Error, stderr.strip.gsub(/\e\[([;\d]+)?m/, '') if status != 0
-  if stdout.strip.nil? || stdout.strip == ""
-    { status: "success" }
+  if verbose == true
+    if stdout.strip.nil? || stdout.strip == ""
+      { status: "success" }
+    else
+      { status: stdout.strip.gsub(/\e\[([;\d]+)?m/, '') }
+    end
   else
-    { status: stdout.strip.gsub(/\e\[([;\d]+)?m/, '') }
+    { status: "success" }
   end
 end
 
 params = JSON.parse(STDIN.read)
 environment = params['environment']
+unless params['verbose']
+  verbose = false
+else
+  verbose = params['verbose']
+end
 
 begin
-  result = generate_types(environment)
+  result = generate_types(environment, verbose)
   puts JSON.pretty_generate(result)
   exit 0
 rescue Puppet::Error => e
